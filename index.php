b5f3b463f3cf087a8e4bb2884e798e33e6b443b8
Alessandro Aglietti <alessandro.aglietti@gmail.com>
2013-08-12 21:01:59 +0200

    aggiunto ga

--- index.php
+++ index.php
@@ -49,6 +49,19 @@
 				background-position-y: 6px;
 			}
 		</style>
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-11408016-2']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
     </head>
     <body style="padding-top: 50px;">
     	<div class="navbar navbar-inverse navbar-fixed-top">
